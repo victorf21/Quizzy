2edc16952a94151bade708d7aedb5ca4f57e2bbd
JudithM05 <2023_judith.mera@iticbcn.cat>
2025-03-07 15:57:18 +0100

    CRUD usuario create hecho

--- API/app/routers/usuario.py
+++ API/app/routers/usuario.py
@@ -10,3 +10,9 @@ from sqlalchemy.orm import Session
 
 router = APIRouter(prefix="/usuarios", tags=["Usuarios"])
 
+@router.post("/", response_model=Usuario)
+def crear_usuario(usuario: Usuario, session: Session = Depends(get_session)):
+    session.add(usuario)
+    session.commit()
+    session.refresh(usuario)
+    return usuario
