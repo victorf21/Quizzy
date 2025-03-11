14cf38ff8ace2e6939099f35cafa4677a0d8bd56
Victor Fernandez <fernandezvictor738@gmail.com>
2025-03-11 15:59:30 +0100

    Modelos cambiados y añadido import de sqlmodel en usuario

--- API/app/database/models.py
+++ API/app/database/models.py
@@ -1,5 +1,6 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional
+import sqlalchemy as sa
 
 class Usuario(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
@@ -17,7 +18,9 @@ class Quiz(SQLModel, table=True):
     id_usuario: Optional[int] = Field(default=None, foreign_key="usuario.id")
     categorias: Optional[str] = None
     num_total_preguntas: Optional[int] = None
-    fecha_creacion: Optional[str] = Field(default="CURRENT_TIMESTAMP")
+    fecha_creacion: Optional[str] = Field(
+        sa_column=sa.Column(sa.TIMESTAMP, server_default=sa.text("CURRENT_TIMESTAMP"))
+    )
     puntuacion: Optional[int] = None
 
 class Pregunta(SQLModel, table=True):
@@ -34,5 +37,7 @@ class Respuesta(SQLModel, table=True):
 class Historial(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     id_usuario: Optional[int] = Field(default=None, foreign_key="usuario.id")
-    fecha_creacion: Optional[str] = Field(default="CURRENT_TIMESTAMP")
+    fecha_creacion: Optional[str] = Field(
+        sa_column=sa.Column(sa.TIMESTAMP, server_default=sa.text("CURRENT_TIMESTAMP"))
+    )
     num_total_preguntas: Optional[int] = None
--- API/app/routers/usuario.py
+++ API/app/routers/usuario.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query, HTTPException
 from database.session import get_session
 from database.models import Usuario
 from sqlalchemy.orm import Session
-
+from sqlmodel import select
 
 router = APIRouter(prefix="/usuarios", tags=["Usuarios"])
 
@@ -12,3 +12,12 @@ def crear_usuario(usuario: Usuario, session: Session = Depends(get_session)):
     session.commit()
     session.refresh(usuario)
     return usuario
+
+@router.get("/")
+def leer_usuarios(
+    session: Session = Depends(get_session),
+    offset: int = 0,
+    limit: int = Query(100, le=100),
+) -> list[Usuario]:
+    usuarios = session.exec(select(Usuario).offset(offset).limit(limit)).all()
+    return usuarios
