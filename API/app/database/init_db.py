e37a7e04f42426c7bb5e083fa544f7071eae63b5
Victor Fernandez <fernandezvictor738@gmail.com>
2025-03-13 15:09:06 +0100

    Cambio de script para hacer migraciones a la base de datos por cambio de modelos

--- API/app/database/init_db.py
+++ API/app/database/init_db.py
@@ -1,5 +1,10 @@
 from database.config import engine
-from .models import SQLModel
+from sqlmodel import SQLModel
+from models.usuario import Usuario
+from models.historial import Historial
+from models.quiz import Quiz
+from models.pregunta import Pregunta
+from models.respuesta import Respuesta
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
