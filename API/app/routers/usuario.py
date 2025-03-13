78ab2bcdff97fe967beb392280599dc9c75667d1
Victor Fernandez <fernandezvictor738@gmail.com>
2025-03-13 15:13:20 +0100

    Cambio de imports por cambio de estructura

--- API/app/routers/usuario.py
+++ API/app/routers/usuario.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, Query, HTTPException
 from database.session import get_session
-from database.models import Usuario
+from ..models.usuario import Usuario
 from sqlalchemy.orm import Session
 from sqlmodel import select
 from schemas.usuario import UsuarioCreate, UsuarioRead
