892be31c0348eaf28cd1d3f2ad8c29b282ac80c7
Victor Fernandez <fernandezvictor738@gmail.com>
2025-03-14 09:36:43 +0100

    CRUD y endepoints de usuario terminados

--- API/app/routers/usuario.py
+++ API/app/routers/usuario.py
@@ -1,25 +1,27 @@
-from fastapi import APIRouter, Depends, Query, HTTPException
+from fastapi import APIRouter, Depends, Query
 from database.session import get_session
-from models.usuario import Usuario
 from sqlalchemy.orm import Session
-from sqlmodel import select
-from schemas.usuario import UsuarioCreate, UsuarioRead
+from schemas.usuario import UsuarioCreate, UsuarioRead, UsuarioUpdate
+from crud.usuario import *
 
 router = APIRouter(prefix="/usuarios", tags=["Usuarios"])
 
-@router.post("/create_usuario", response_model=UsuarioRead)
+@router.post("/", response_model=UsuarioRead)
 def crear_usuario(usuario: UsuarioCreate, session: Session = Depends(get_session)):
-    nuevo_usuario = Usuario(**usuario.model_dump()) 
-    session.add(nuevo_usuario)
-    session.commit()
-    session.refresh(nuevo_usuario)
-    return nuevo_usuario
+    return create_usuario(session, usuario)
 
-@router.get("/")
-def leer_usuarios(
-    session: Session = Depends(get_session),
-    offset: int = 0,
-    limit: int = Query(100, le=100),
-) -> list[Usuario]:
-    usuarios = session.exec(select(Usuario).offset(offset).limit(limit)).all()
-    return usuarios
+@router.get("/", response_model=list[UsuarioRead])
+def leer_usuarios(session: Session = Depends(get_session), offset: int = 0, limit: int = Query(100, le=100)):
+    return get_usuarios(session, offset, limit)
+
+@router.get("/{usuario_id}", response_model=UsuarioRead)
+def leer_usuario(usuario_id: int, session: Session = Depends(get_session)):
+    return get_usuario(session, usuario_id)
+
+@router.put("/{usuario_id}", response_model=UsuarioRead)
+def actualizar_usuario(usuario_id: int, usuario_update: UsuarioUpdate, session: Session = Depends(get_session)):
+    return update_usuario(session, usuario_id, usuario_update)
+
+@router.delete("/{usuario_id}")
+def eliminar_usuario(usuario_id: int, session: Session = Depends(get_session)):
+    return delete_usuario(session, usuario_id)
