bc8cdfb2b94446001b306f73548a57e675142d09
JudithM05 <2023_judith.mera@iticbcn.cat>
2025-04-24 15:12:08 +0200

    Añadidas fotos de perfil de usuario

--- API/app/database/config.py
+++ API/app/database/config.py
@@ -2,7 +2,7 @@ from sqlmodel import create_engine
 import os
 # Configuración de PostgreSQL
 
-DATABASE_URL = "postgresql://postgres:root@localhost:5432/trivia_db?client_encoding=utf8"
+DATABASE_URL = "postgresql://postgres:pirineus@localhost:5432/trivia_db?client_encoding=utf8"
 # Crear motor de base de datos
 engine = create_engine(DATABASE_URL, echo=True)
 
